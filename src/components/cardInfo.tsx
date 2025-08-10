f3ff2816673e6c4e46596ba44c6ec839d7fabed8
cadudml2707 <dcaduardo@gmail.com>
2025-08-09 23:47:12 -0400

    Listagem das compras vindo do banco

---
+++ src/components/cardInfo.tsx
@@ -0,0 +1,30 @@
+import { Text, View } from 'react-native';
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
+
+type CardInfoProps = {
+  nome: string;
+  preco: string;
+  dataVencimento: string;
+  status: number;
+};
+
+export function CardInfo({ nome, preco, dataVencimento, status }: CardInfoProps) {
+  const corStatus = status === 1 ? '[#13C782]' : status === 3 ? '[#FB4646]' : '[#262429]';
+  const textoStatus = status === 1 ? 'pago' : 'pagar';
+  const corTexto = status === 2 ? '#13C782' : 'white';
+  const corVencimento = status === 1 ? '#13C782' : 'white';
+
+  return (
+    <View className={`flex-row justify-between p-6 gap-2 rounded-2xl bg-${corStatus}`}>
+      <View className="flex-row gap-10">
+        <FontAwesome5 name="receipt" size={35} color="white" />
+        <View>
+          <Text className="font-bold text-white text-lg">{nome}</Text>
+          <Text className="text-base text-white">{preco}</Text>
+          <Text className={`text-sm mt-2`} style={{ color: corVencimento}}>Vencimento: {dataVencimento}</Text>
+        </View>
+      </View>
+      <Text className="font-bold text-lg" style={{ color: corTexto }}>{textoStatus}</Text>
+    </View>
+  );
+}
--- src/components/listaCompras.tsx
+++ src/components/listaCompras.tsx
@@ -1,130 +1,150 @@
-import { useState } from "react";
-import { Text, View, TouchableOpacity, ScrollView } from 'react-native';
+import { useEffect, useState } from "react";
+import { Text, View, TouchableOpacity } from 'react-native';
 import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
+import { mostraCompras } from "../models/formularioModel";
+import { CardInfo } from "./cardInfo";
 
-export function ListaCompras() {
+export function ListaCompras({ navigation }: any) {
+
+    const [compras, setCompras] = useState<any[]>([]);
+
+    useEffect(() => {
+        async function pegaCompras() {
+            const resultado = await mostraCompras();
+            setCompras(resultado);
+        }
+        pegaCompras();
+    }, []);
 
     const [aba, setAba] = useState(1);
 
     function handleMudarCor(cor: number) {
-        switch (cor) {
-            case 1:
-                setAba(1);
-                break;
-            case 2:
-                setAba(2);
-                break;
-            case 3:
-                setAba(3);
-                break;
-            case 4:
-                setAba(4);
-                break;
-        }
+        setAba(cor);
     }
 
-    return (
-        <View>
-            <View className='flex-row border-b-2 border-b-[#13C782] p-2 justify-between'>
-                <TouchableOpacity onPress={() => handleMudarCor(1)}>
-                    <Text className="text-lg" style={{ color: aba === 1 ? '#13C782' : '#E0E0E09E', }}>Todas</Text>
-                </TouchableOpacity>
-                <TouchableOpacity onPress={() => handleMudarCor(2)}>
-                    <Text className="text-lg" style={{ color: aba === 2 ? '#13C782' : '#E0E0E09E', }}>Pagas</Text>
-                </TouchableOpacity>
-                <TouchableOpacity onPress={() => handleMudarCor(3)}>
-                    <Text className="text-lg" style={{ color: aba === 3 ? '#13C782' : '#E0E0E09E', }}>Atrasadas</Text>
-                </TouchableOpacity>
-                <TouchableOpacity onPress={() => handleMudarCor(4)}>
-                    <Text className="text-lg" style={{ color: aba === 4 ? '#13C782' : '#E0E0E09E', }}>Andamento</Text>
-                </TouchableOpacity>
+    function handleEditar(){
+        navigation.navigate("Editar");
+    }
+
+    if (compras.length === 0) {
+        return (
+            <View>
+                <View className='flex-row border-b-2 border-b-[#13C782] p-2 justify-between'>
+                    <TouchableOpacity onPress={() => handleMudarCor(1)}>
+                        <Text className="text-lg" style={{ color: aba === 1 ? '#13C782' : '#E0E0E09E', }}>Todas</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => handleMudarCor(2)}>
+                        <Text className="text-lg" style={{ color: aba === 2 ? '#13C782' : '#E0E0E09E', }}>Pagas</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => handleMudarCor(3)}>
+                        <Text className="text-lg" style={{ color: aba === 3 ? '#13C782' : '#E0E0E09E', }}>Atrasadas</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => handleMudarCor(4)}>
+                        <Text className="text-lg" style={{ color: aba === 4 ? '#13C782' : '#E0E0E09E', }}>Andamento</Text>
+                    </TouchableOpacity>
+                </View>
+                <Text className="text-white text-center mt-6">Nenhuma compra cadastrada.</Text>
             </View>
-            {aba === 1 && (
-                <View className="gap-4 flex-2">
-                    <Text className="mt-8 text-lg" style={{ color: '#A2A2A2' }}>Histórico do dia (data)</Text>
-                    <View className="flex-row justify-between p-6  gap-2 rounded-2xl" style={{ backgroundColor: '#262429' }}>
-                        <View className="flex-row gap-10">
-                            <FontAwesome5 name="receipt" size={30} color="white" />
-                            <View>
-                                <Text className="font-bold text-white text-lg">Nome</Text>
-                                <Text className="text-base" style={{ color: '#C1C1C1' }}>Preço</Text>
-                                <Text className="text-white text-sm mt-4">vencimento:(data)</Text>
-                            </View>
-                        </View>
-                        <Text className="font-bold text-lg" style={{ color: '#13C782' }}>pagar</Text>
-                    </View>
-                    <Text className="mt-8 text-lg" style={{ color: '#A2A2A2' }}>Histórico do dia (data)</Text>
-                    <View className="flex-row justify-between p-6 bg-[#11C884] gap-2 rounded-2xl">
-                        <View className="flex-row gap-10">
-                            <FontAwesome5 name="receipt" size={30} color="white" />
-                            <View>
-                                <Text className="font-bold text-white text-lg">Nome</Text>
-                                <Text className="text-base text-white">Preço</Text>
-                            </View>
-                        </View>
-                        <Text className="font-bold text-lg text-white">pago</Text>
-                    </View>
-                    <Text className="mt-8 text-lg" style={{ color: '#A2A2A2' }}>Histórico do dia (data)</Text>
-                    <View className="flex-row justify-between p-6 bg-[#FB4646] gap-2 rounded-2xl">
-                        <View className="flex-row gap-10">
-                            <FontAwesome5 name="receipt" size={30} color="white" />
-                            <View>
-                                <Text className="font-bold text-white text-lg">Nome</Text>
-                                <Text className="text-base text-white">Preço</Text>
-                                <Text className="text-white text-sm mt-4">Atrasada</Text>
-                            </View>
-                        </View>
-                        <Text className="font-bold text-lg text-white">pago</Text>
-                    </View>
+        )
+    } else {
+        return (
+            <View>
+                <View className='flex-row border-b-2 border-b-[#13C782] p-2 justify-between'>
+                    <TouchableOpacity onPress={() => handleMudarCor(1)}>
+                        <Text className="text-lg" style={{ color: aba === 1 ? '#13C782' : '#E0E0E09E', }}>Todas</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => handleMudarCor(2)}>
+                        <Text className="text-lg" style={{ color: aba === 2 ? '#13C782' : '#E0E0E09E', }}>Pagas</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => handleMudarCor(3)}>
+                        <Text className="text-lg" style={{ color: aba === 3 ? '#13C782' : '#E0E0E09E', }}>Atrasadas</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => handleMudarCor(4)}>
+                        <Text className="text-lg" style={{ color: aba === 4 ? '#13C782' : '#E0E0E09E', }}>Andamento</Text>
+                    </TouchableOpacity>
                 </View>
-            )}
-            {aba === 2 && (
-                <View className="gap-4">
-                    <Text className="mt-8 text-lg" style={{ color: '#A2A2A2' }}>Histórico do dia (data)</Text>
-                    <View className="flex-row justify-between p-6 bg-[#11C884] gap-2 rounded-2xl">
-                        <View className="flex-row gap-10">
-                            <FontAwesome5 name="receipt" size={30} color="white" />
-                            <View>
-                                <Text className="font-bold text-white text-lg">Nome</Text>
-                                <Text className="text-base text-white">Preço</Text>
-                            </View>
-                        </View>
-                        <Text className="font-bold text-lg text-white">pago</Text>
+                {aba === 1 && (
+                    <View className="gap-4 flex-2">
+                        <Text className="mt-8 text-lg" style={{ color: '#A2A2A2' }}>Histórico do dia (data)</Text>
+                        {compras.map((compra) => (
+                            <CardInfo
+                                key={compra.id}
+                                nome={compra.nome}
+                                preco={compra.preco}
+                                dataVencimento={compra.dataVencimento}
+                                status={compra.status}
+                            />
+                        ))}
+                        <Text className="mt-8 text-lg" style={{ color: '#A2A2A2' }}>Histórico do dia (data)</Text>
+
+                        <Text className="mt-8 text-lg" style={{ color: '#A2A2A2' }}>Histórico do dia (data)</Text>
+
                     </View>
-                </View>
-            )}
-            {aba === 3 && (
-                <View className="gap-4">
-                    <Text className="mt-8 text-lg" style={{ color: '#A2A2A2' }}>Histórico do dia (data)</Text>
-                    <View className="flex-row justify-between p-6 bg-[#FB4646] gap-2 rounded-2xl">
-                        <View className="flex-row gap-10">
-                            <FontAwesome5 name="receipt" size={30} color="white" />
-                            <View>
-                                <Text className="font-bold text-white text-lg">Nome</Text>
-                                <Text className="text-base text-white">Preço</Text>
-                                <Text className="text-white text-sm mt-4">Atrasada</Text>
-                            </View>
-                        </View>
-                        <Text className="font-bold text-lg text-white">pago</Text>
+                )}
+                {aba === 2 && (
+                    <View className="gap-4 flex-2">
+                        <Text className="mt-8 text-lg" style={{ color: '#A2A2A2' }}>Histórico do dia (data)</Text>
+                        {compras.map((compra) => (
+                            compra.status === 1 && (
+                                <CardInfo 
+                                key={compra.id}
+                                nome={compra.nome}
+                                preco={compra.preco}
+                                dataVencimento={compra.dataVencimento}
+                                status={compra.status}
+                            />
+                            )
+                        ))}
+                        {compras.map((compra) => (
+                            compra.length === 0 && (
+                                <Text className="text-white text-center mt-6">Nenhuma compra paga.</Text>
+                            )
+                        ))}
                     </View>
-                </View>
-            )}
-            {aba === 4 && (
-                <View className="gap-4">
-                    <Text className="mt-8 text-lg" style={{ color: '#A2A2A2' }}>Histórico do dia (data)</Text>
-                    <View className="flex-row justify-between p-6  gap-2 rounded-2xl" style={{ backgroundColor: '#262429' }}>
-                        <View className="flex-row gap-10">
-                            <FontAwesome5 name="receipt" size={30} color="white" />
-                            <View>
-                                <Text className="font-bold text-white text-lg">Nome</Text>
-                                <Text className="text-base" style={{ color: '#C1C1C1' }}>Preço</Text>
-                                <Text className="text-white text-sm mt-4">vencimento:(data)</Text>
-                            </View>
-                        </View>
-                        <Text className="font-bold text-lg" style={{ color: '#13C782' }}>pagar</Text>
+                )}
+                {aba === 3 && (
+                    <View className="gap-4">
+                        <Text className="mt-8 text-lg" style={{ color: '#A2A2A2' }}>Histórico do dia (data)</Text>
+                        {compras.map((compra) => (
+                            compra.status === 3 && (
+                                <CardInfo 
+                                key={compra.id}
+                                nome={compra.nome}
+                                preco={compra.preco}
+                                dataVencimento={compra.dataVencimento}
+                                status={compra.status}
+                            />
+                            )
+                        ))}
+                        {compras.map((compra) => (
+                            compra.length === 0 && (
+                                <Text className="text-white text-center mt-6">Nenhuma compra atrasada.</Text>
+                            )
+                        ))}
                     </View>
-                </View>
-            )}
-        </View>
-    )
+                )}
+                {aba === 4 && (
+                    <View className="gap-4">
+                        <Text className="mt-8 text-lg" style={{ color: '#A2A2A2' }}>Histórico do dia (data)</Text>
+                        {compras.map((compra) => (
+                            compra.status === 2 && (
+                                <CardInfo 
+                                key={compra.id}
+                                nome={compra.nome}
+                                preco={compra.preco}
+                                dataVencimento={compra.dataVencimento}
+                                status={compra.status}
+                            />
+                            )
+                        ))}
+                        {compras.map((compra) => (
+                            compra.length === 0 && (
+                                <Text className="text-white text-center mt-6">Nenhuma compra em andamento.</Text>
+                            )
+                        ))}
+                    </View>
+                )}
+            </View>
+        )
+    }
 }
